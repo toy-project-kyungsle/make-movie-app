65e682a183233ba2693deea029b1d5eb7f863d48
keinn51 <soogood97@gmail.com>
2021-12-10 00:23:11 +0900

    7_4_END

--- create_react_app/src/App.js
+++ create_react_app/src/App.js
@@ -1,65 +1,22 @@
-import {useEffect, useState}  from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Home from "./routes/Home"
+import Detail from './routes/Detail';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [enterButton, setEnterButton] = useState(false);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [buyList, setBuyList] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  const onChange = (event) => {
-    setMoney(event.target.value);
-    setEnterButton((curr) => (curr = false));
-  }
-
-  const onClick = () => {
-    setBuyList(coins.filter((coins) => money > coins.quotes.USD.price))
-    setEnterButton((curr) => (curr = true));
-  }
-
   return (
-    <div>
-      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
-      {
-        loading 
-        ? <strong>Loding!</strong> 
-        : 
-        <div>
-          <h2>Enter Your Money (USD)</h2>
-          <input
-            type="text"
-            placeholder="money"
-            value={money}
-            onChange={onChange}
-          ></input>
-          <button onClick={onClick}>Search!</button>
-
-          {
-            enterButton
-            ? 
-            <div>
-              <h2>You Can Buy! ({buyList.length})</h2>
-              <ul>
-                {buyList.map((coins) => <li key={coins.id}>
-                  {coins.name} === {Math.floor(money / coins.quotes.USD.price)} ({coins.symbol}) 
-                  </li>)}
-              </ul>
-            </div>
-            : null
-          }
-        </div>
-      }
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/movie" element={<Detail />}>
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
