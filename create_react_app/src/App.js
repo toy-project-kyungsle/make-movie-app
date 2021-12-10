5837da3380eb6cfef3e6e3dce0abef684f29fe10
keinn51 <soogood97@gmail.com>
2021-12-10 15:53:22 +0900

    All_DONE

--- create_react_app/src/App.js
+++ create_react_app/src/App.js
@@ -8,11 +8,11 @@ import Detail from './routes/Detail';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />}>
         </Route>
-        <Route path="/movie" element={<Detail />}>
+        <Route path="/movie/:id" element={<Detail />}>
         </Route>
       </Routes>
     </Router>
--- create_react_app/src/components/Movie.js
+++ create_react_app/src/components/Movie.js
@@ -1,20 +1,35 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom"
 
-function Movie({coverImg, title, summary, genres}) {
+function Movie({id, coverImg, rating, runtime, description_full,title, summary, genres}) {
   return (
     <div>
       <img src={coverImg} alt={title}/>
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
-      <ul>{genres.map(g => <li key={g}>{g}</li>)}</ul>
+      <div>
+        <p>{rating && `rating: ${rating}`}</p>
+        <p>{runtime && `runtime: ${runtime} (min)`}</p>
+      </div>
+      <div>
+        <b>{summary && '<summary>'}</b>
+        <p>{summary}</p>
+      </div>
+      <div>
+        <b>{description_full && '<description>'}</b>
+        <p>{description_full}</p>
+      </div>
+      <div>
+        <b>{genres && '<genres>'}</b>
+        <ul>{genres && genres.map(g => <li key={g}>{g}</li>)}</ul>
+      </div>
     </div>
   )
 }
 
 Movie.prototypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
