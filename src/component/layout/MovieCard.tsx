4bca46f9b5b037471ab1e928e7f260a0a322bfd9
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-05-15 22:45:51 +0900

    ✨ [Feat] movie card 컴포넌트 추가 (스타일도)

---
+++ src/component/layout/MovieCard.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import styles from '@/style/movieCard.module.scss';
+import Image from 'next/image';
+
+interface MovieCardProps {
+  id: number;
+  coverImg: string;
+  rating: number;
+  runtime: number;
+  title: string;
+}
+
+const MovieCard = ({ id, coverImg, rating, runtime, title }: MovieCardProps) => {
+  return (
+    <div className={styles.movie}>
+      <Link href={`/detail?id=${id}`}>
+        <img src={coverImg} alt={title} />
+        <div className={styles.letters}>
+          <div className={styles.title}>
+            <div>
+              <h3 className={styles.movieName}>
+                {title.length > 35 ? `${title.slice(0, 35)}...` : title}
+              </h3>
+            </div>
+          </div>
+          <span>{rating ? `rating: ${rating} / 10` : null}</span>
+          <p>{runtime ? `runtime: ${runtime} (min)` : null}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/component/layout/Slide.tsx
+++ src/component/layout/Slide.tsx
@@ -3,17 +3,34 @@ import { useQuery } from 'react-query';
 import styles from '@/style/slide.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretSquareLeft, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MovieDataType } from '@/type/movie';
+import MovieCard from './MovieCard';
 
 const Slide = () => {
   const {
     isLoading,
     error,
     data: movieData,
-  } = useQuery<MovieDataType>('myQueryKey', () =>
+  } = useQuery<MovieDataType[]>('myQueryKey', () =>
     getMoviesFromServer({ limit: '10', sort_by: 'rating' }),
   );
+
+  const [trans, setTrans] = useState(0);
+
+  // >= 0 : The End of the Right!
+  const onClickL = () => {
+    if (trans >= 0) return;
+
+    setTrans((current) => current + 460);
+  };
+
+  // -1380 : 230 * 6, so the button can be clicked only 3 times
+  const onClickR = () => {
+    if (trans <= -1380) return;
+
+    setTrans((current) => current - 460);
+  };
 
   useEffect(() => {
     console.log(movieData);
@@ -22,30 +39,31 @@ const Slide = () => {
   return (
     <div className={styles.container}>
       <div className={styles.slide_show}>
-        {/* <div className={styles.slides} style={{ transform: `translateX(${trans}px)` }}>
-          {movies.map((movie) => {
-            if (movie.medium_cover_image != null) {
-              return (
-                // MovieSlide is redering code with Slide!
-                <MovieSlide
-                  key={movie.id}
-                  id={movie.id}
-                  coverImg={movie.medium_cover_image}
-                  rating={movie.rating}
-                  runtime={movie.runtime}
-                  title={movie.title}
-                />
-              );
-            }
-          })}
-        </div> */}
+        <div className={styles.slides} style={{ transform: `translateX(${trans}px)` }}>
+          {movieData &&
+            movieData.map((movie) => {
+              if (movie.medium_cover_image != null) {
+                return (
+                  // MovieSlide is redering code with Slide!
+                  <MovieCard
+                    key={movie.id}
+                    id={movie.id}
+                    coverImg={movie.medium_cover_image}
+                    rating={movie.rating}
+                    runtime={movie.runtime}
+                    title={movie.title}
+                  />
+                );
+              }
+            })}
+        </div>
       </div>
 
       <div className={styles.controller}>
-        <button className={styles.left} onClick={() => {}}>
+        <button className={styles.left} onClick={onClickL}>
           <FontAwesomeIcon icon={faCaretSquareLeft}></FontAwesomeIcon>
         </button>
-        <button className={styles.right} onClick={() => {}}>
+        <button className={styles.right} onClick={onClickR}>
           <FontAwesomeIcon icon={faCaretSquareRight}></FontAwesomeIcon>
         </button>
       </div>
